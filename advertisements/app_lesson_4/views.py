636e81ef2c1ac1de8450489b88a3c655cc8b9e11
Yarik <yariksuhan@gmail.com>
2023-07-09 17:06:25 +0400

    lesson_4_dz

--- advertisements/app_lesson_4/views.py
+++ advertisements/app_lesson_4/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Домашка по 4 занятию.")
+def lesson_4(request):
+    return HttpResponse('Домашка по 4 занятию')
