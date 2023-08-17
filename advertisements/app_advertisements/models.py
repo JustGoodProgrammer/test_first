d73f29b31ef83c4dd39bc253004e88d301e4c8ff
Yarik <yariksuhan@gmail.com>
2023-08-17 15:22:12 +0400

    Наполнение шаблонов

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        verbose_name='пользователь',
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        'Изображение',
+        upload_to='advertisments/'
+    )
+
     # Товар
     # строковое поле для небольших размеров
     # 'заголовок' - verbose_name - название поля извне
