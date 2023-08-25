1cefa813d95dfc2e52553da3c182a43d17e697bc
Yarik <yariksuhan@gmail.com>
2023-08-25 15:45:37 +0400

    test

--- advertisements/app_advertisements/forms.py
+++ advertisements/app_advertisements/forms.py
@@ -1,9 +1,50 @@
 from django import forms
 
 
-class AdvertisementForm(forms.Form):
+'''class AdvertisementForm(forms.Form):
     title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     auction = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), required=True)
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))'''
+
+'''from django.db import models
+from django.forms import CheckboxInput, ModelForm, Textarea, FileInput
+
+class Models(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length = None, widget = Textarea(attrs={'class': 'form-control'}))
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    auction = models.BooleanField(widget = CheckboxInput(attrs={'class': 'form-check-input'}), required = True)
+    image = models.ImageField(widget = FileInput(attrs={'class': 'form-control'}))
+
+    def __str__(self): 
+        return self.title
+
+class AdvertisementForm(ModelForm):
+    class Meta:
+        model = Models
+        fields = ['__all__']'''
+
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from .models import Advertisement
+
+def validation_question(value):
+    if value[0] == '?':
+        raise ValidationError(
+            ('%(value)s starts with ?, that not allowed'),
+            params={'value': value},
+        )
+ 
+class AdvertisementForm(ModelForm):
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'description', 'price', 'auction', 'image']
+        widgets = {
+            'title': forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}), validators=[validation_question]),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'auction': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
+        }
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-for i in range(int(input())):
+'''for i in range(int(input())):
     a = int(input())
     b = bin(a-1)[2:]
     if '111' in b:
         b = b[b.rfind('1')-2:]
         print(int(b, base=2))
     else:
-        print(-1)
+        print(-1)'''
+a = int(input())
+l = [int(i) for i in input().split()]
+ans = []
+
+if a % 2 == 0:
+    print('no')
+else:
+    for i in range(a//2):
+        b = max(l)
+        ans.append(b)
+        l.remove(b)
+    if ans[-1] > max(l):
+        print('yes')
+    else:
+        print('no')
